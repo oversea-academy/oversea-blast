99ff7101ad680c63d1902920b32844e6e9ad7902
Miftahul Arifin <miftahul97@gmail.com>
2022-03-02 17:39:09 +0700

    fix: update database

--- loaders/sequelize.js
+++ loaders/sequelize.js
@@ -3,10 +3,15 @@ const model 		= require('../models');
 
 module.exports = () => {
 	return new Promise(async (resolve) => {
-		const db = new Sequelize(process.env.DATABASE_URL, {
-			logging : process.env.DATABASE_LOGGING ? console.log : false,
-			native  : true, 
-			ssl     : true
+		const db = new Sequelize({
+			host: process.env.DB_HOST,
+			database: process.env.DB_NAME,
+			username: process.env.DB_USER,
+			password: process.env.DB_PASS,
+			port: parseInt(process.env.DB_PORT, 10) || 5432,
+			dialect: 'postgres',
+			logging : process.env.DB_LOG ? console.log : false,
+			timezone: 'Asia/Jakarta'
 		});
 		
 		const user 	= model.user(db);
